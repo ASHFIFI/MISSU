6aac7e0a0ff77b5b1959a7a1edaf4846e41beb93
MDXC <1442872865@qq.com>
2019-06-26 11:27:06 +0800

    2019/6/26 add some libraries

--- app/src/main/java/com/missu/MainActivity.java
+++ app/src/main/java/com/missu/MainActivity.java
@@ -3,11 +3,14 @@ package com.missu;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.jaeger.library.StatusBarUtil;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StatusBarUtil.setTransparent(this);
     }
 }
